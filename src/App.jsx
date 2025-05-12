5ad23cc3d996e09c9ee9fb7463ba7ca6158111e7
saudlari <larissa@riseup.net>
2025-05-12 17:23:37 +0200

    feat(Hero) : base structure Hiro section

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import NavBar from './components/NavBar'
+import HeroSection from './components/HeroSection'
+
+
+import './App.css'
+
+function App() {  
+  
+  return (
+    <>
+      <div>
+        <NavBar />
+        <HeroSection />
+
+    </div>
+    </>
+  )
+}
+
+export default App
