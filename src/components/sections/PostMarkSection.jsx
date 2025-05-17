e044df84c6f243727fba6cfac064d3c42964a8c5
saudlari <larissa@riseup.net>
2025-05-17 16:48:42 +0200

    feat (mark) : implement title, description, and upload img in mark form

--- src/components/sections/PostMarkSection.jsx
+++ src/components/sections/PostMarkSection.jsx
@@ -1,11 +1,34 @@
-import React from 'react';
+import React from "react";
 
 const PostMarkSection = () => {
-    return (
-        <div>
-            <h1>PostMarkSection</h1>
+  return (
+    <div className="flex justify-center items-center min-h-screen">
+      <div className="card bg-base-100 w-96 shadow-sm">
+        <div className="card-body items-center text-center">
+          <h2 className="card-title">Publicar</h2>
+          <p>
+            Publica tu idea, propuesta o iniciativa para que la comunidad la
+            pueda ver y apoyar.
+          </p>
+
+          <input type="file" className="file-input file-input-secondary" />
+
+          <input
+            type="text"
+            placeholder="¿Qué idea quieres compartir?"
+            className="input input-secondary"
+          />
+
+          <textarea
+            className="textarea textarea-secondary"
+            placeholder="¿Cuál es la idea?"
+          ></textarea>
+
+         
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default PostMarkSection;
