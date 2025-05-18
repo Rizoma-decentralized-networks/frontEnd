e0af3a9cd88cf2dadf7e7559d2ef9270ceb4f6f0
saudlari <larissa@riseup.net>
2025-05-18 15:47:52 +0200

    feat (Mark) : create a post mark form with daisy and onsubmit data structure

--- src/components/sections/PostMarkSection.jsx
+++ src/components/sections/PostMarkSection.jsx
@@ -1,34 +1,127 @@
-import React from "react";
+import { useForm } from "react-hook-form";
+import PublishMark from "../buttons/PublishMark";
 
-const PostMarkSection = () => {
+function PostMarkForm() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (dataMark) => {
+    const structuredDataMark = {
+      file: dataMark.file[0], 
+      category: dataMark.category,
+      type: dataMark.type,
+      title: dataMark.title,
+      description: dataMark.description,
+      location: dataMark.location,
+    };
+    console.log(structuredDataMark);
+  };
+
   return (
-    <div className="flex justify-center items-center min-h-screen">
-      <div className="card bg-base-100 w-96 shadow-sm">
-        <div className="card-body items-center text-center">
-          <h2 className="card-title">Publicar</h2>
-          <p>
-            Publica tu idea, propuesta o iniciativa para que la comunidad la
-            pueda ver y apoyar.
-          </p>
+    <form
+      onSubmit={handleSubmit(onSubmit)}
+      className="flex flex-col gap-4 items-center justify-center min-h-[70vh] bg-gray-100"
+    >
+      <div className="bg-white shadow-md p-8 rounded-md w-80 text-center">
+        <h1 className="text-lg mb-6">Create Bookmark</h1>
 
-          <input type="file" className="file-input file-input-secondary" />
+        <input
+          type="file"
+          {...register("file")}
+          className="file-input file-input-secondary mb-3"
+        />
 
-          <input
-            type="text"
-            placeholder="¿Qué idea quieres compartir?"
-            className="input input-secondary"
-          />
+        <div className="flex gap-4 mb-3">
+          <div className="flex-1">
+            <select
+              {...register("category", { required: true })}
+              className="select validator select-secondary"
+              defaultValue=""
+            >
+              <option disabled value="">
+                Category
+              </option>
+              <option>Proposals</option>
+              <option>Initiatives</option>
+              <option>Conflicts</option>
+            </select>
+            {errors.category && (
+              <p className="text-red-500 text-sm">
+                A category must be selected
+              </p>
+            )}
+          </div>
 
-          <textarea
-            className="textarea textarea-secondary"
-            placeholder="¿Cuál es la idea?"
-          ></textarea>
-
-         
+          <div className="flex-1">
+            <select
+              {...register("type", { required: true })}
+              className="select validator select-secondary"
+              defaultValue=""
+            >
+              <option disabled value="">
+                Tag
+              </option>
+              <option>Environment</option>
+              <option>Feminist</option>
+              <option>Public Service</option>
+              <option>Tenement</option>
+              <option>Urbanism</option>
+              <option>Mobility</option>
+              <option>Culture</option>
+              <option>Enconomy and employment</option>
+              <option>Sport</option>
+              <option>Democracy memory</option>
+            </select>
+            {errors.type && (
+              <p className="text-red-500 text-sm">A type must be selected</p>
+            )}
+          </div>
         </div>
-      </div>
-    </div>
-  );
-};
 
-export default PostMarkSection;
+        <input
+          type="text"
+          {...register("title")}
+          placeholder="Title"
+          className="input input-secondary mb-3"
+        />
+        <input
+          type="text"
+          {...register("description")}
+          placeholder="Description"
+          className="textarea textarea-secondary mb-3"
+        />
+
+        <label className="input flex items-center gap-2 mb-4 input-secondary">
+          <svg
+            className="h-[1em] opacity-50"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+          >
+            <g
+              strokeLinejoin="round"
+              strokeLinecap="round"
+              strokeWidth="2.5"
+              fill="none"
+              stroke="currentColor"
+            >
+              <circle cx="11" cy="11" r="8"></circle>
+              <path d="m21 21-4.3-4.3"></path>
+            </g>
+          </svg>
+          <input
+            type="search"
+            {...register("location")}
+            className="grow"
+            placeholder="Location"
+          />
+        </label>
+        <PublishMark />
+      </div>
+    </form>
+  );
+}
+
+export default PostMarkForm;
